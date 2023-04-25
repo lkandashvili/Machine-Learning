8debaf5e0cc0db25324fb5e79821e1572faa2baa
Lasha Kandashvili <lasha.kandashvili.1@btu.edu.ge>
2023-04-25 19:42:50 +0400

    daemata Quiz_2 da Shabloni

---
+++ Testaoba/Shabloni.py
@@ -0,0 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC, SVC
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("blabla.svc")
+data[]
