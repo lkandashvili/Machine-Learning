b18c206a7484b3514e30667a8878b5cf11dfac65
Lasha Kandashvili <lasha.kandashvili.1@btu.edu.ge>
2023-05-18 14:15:48 +0400

    added LasuraQUIZ3

---
+++ TestaobaQuiz3.py
@@ -0,0 +1,35 @@
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
+from sklearn.datasets import make_classification
+from collections import Counter
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+
+# X, y = make_classification(n_samples=6000, n_features=20, n_redundant=18, n_informative=2, weights=[0.95])
+# X, y = RandomUnderSampler().fit_resample(X, y)
+# print(Counter(y))
+
+documents = ["This little kitty came to play when I was eating at a restaurant.",
+             "Merley has the best squooshy kitten belly.",
+             "Google Translate app is incredible.",
+             "If you open 100 tab in google you get a smiley face.",
+             "Best cat photo I've ever taken.",
+             "Climbing ninja cat.",
+             "Impressed with google map feedback.",
+             "Key promoter extension for Google Chrome."]
+
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(documents)
+true_k = 2
+
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+model.fit(X)
+
+Y = vectorizer.transform(["I love cats."])
+prediction = model.predict(Y)
+print(prediction)
+
+
+
